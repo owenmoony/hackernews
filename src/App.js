fd218c9bdde88fb2e272fd1853191dd682860483
Owen Moony <owenmoony@gmail.com>
2019-03-17 14:19:19 +0100

    As reusabel components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -38,7 +38,7 @@ class App extends Component {
   }
 
   onSearchChange = (event) => {
-    this.setState({ searchTerm: event.target.value})
+    this.setState({ searchTerm: event.target.value })
   }
 
   onDismiss = (id) => {
@@ -47,48 +47,74 @@ class App extends Component {
       return item.objectID != id;
     })
     const updatedList = this.state.list.filter(isNotId);
-    this.setState({list: updatedList})
+    this.setState({ list: updatedList })
   }
 
   render() {
     const helloWorld = "Welcome to the App";
-    const {list, searchTerm} = this.state
-    return (
-      <div className="App">
-        <h2>{helloWorld}</h2>
-        <form>
-          <input
-            type="text"
-            onChange={this.onSearchChange}
-          />
-        </form>
-        {list.filter(isSearched(searchTerm)).map(item => {
-            const onDismissHandler = () =>
-              this.onDismiss(item.objectID);
+    const { list, searchTerm } = this.state
 
-            return (
-              <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-                <span>{item.author}</span>
-                <span>{item.num_comments}</span>
-                <span>{item.points}</span>
-                <span>
-              <button onClick={onDismissHandler}
-                      type="button"
-              >
-                Dismiss
+    return (
+      <div className='App'>
+        <Search
+          value={searchTerm}
+          onChange={this.onSearchChange}
+        />
+        <Table
+          list={list}
+          pattern={searchTerm}
+          onDismiss={this.onDismiss}
+        />
+      </div>
+    );
+  }
+}
+
+class Search extends Component {
+  render() {
+    const { value, onChange } = this.props;
+    return (
+      <form>
+        <input
+          type="text"
+          value={value}
+          onChange={onChange}
+        />
+      </form>
+    );
+  }
+}
+
+class Table extends Component {
+  render() {
+    const { list, pattern, onDismiss } = this.props;
+    return (
+      <div>
+        {list.filter(isSearched(pattern)).map(item => {
+          return (
+            <div key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+              <span>
+                <button
+                  onClick={() => onDismiss(item.objectID)}
+                  type="button">
+                  Dismiss
               </button>
-            </span>
-              </div>
-            )
-          }
+              </span>
+            </div>
+          )
+        }
         )}
       </div>
     );
   }
 }
+
 
 export default App;
 
